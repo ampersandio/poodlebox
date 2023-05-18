e0fb55d4e019429667f79f7234121076b236e1f7
Nikolai Dimitrov <nikolaisyl@gmail.com>
2023-05-18 08:50:46 +0300

    feat config

---
+++ config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = 'Dr. Blalexolai E-University'
+    secret_key: str
+    access_token_expires_minutes: int
+    algorithm: str
+    database: str
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: int
+
+    class Config:
+        env_file = '.env'
+        
