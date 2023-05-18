ec62b574e8bd8df3dd7710db8cf38fd217af8691
Nikolai Dimitrov <nikolaisyl@gmail.com>
2023-05-18 09:56:55 +0300

    feat database config

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Settings(BaseSettings):
     access_token_expires_minutes: int
     algorithm: str
     database: str
+    db_host: str
     db_user: str
     db_password: str
-    db_host: str
     db_port: int
 
     class Config:
