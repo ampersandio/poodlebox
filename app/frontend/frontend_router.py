49f06b7aa52f1b4767c32249440116c013ad04b3
anedelev <anedelev@typedepot.com>
2023-05-19 17:04:19 +0300

    feat: frontend endpoints, header authentication

--- app/frontend/frontend_router.py
+++ app/frontend/frontend_router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends, Form, HTTPException
 from typing import Annotated
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from api.data.models import User
-from api.services.authorization import get_current_user
+from api.services.authorization import get_current_user, create_access_token, authenticate_user
+
 import requests
 
 
@@ -14,10 +15,21 @@ templates = Jinja2Templates(directory="frontend/templates")
 
 @frontend_router.get("/")
 def index(request:Request):
-    print(request)
-    courses = requests.get("http://localhost:8002/api/authorization/test")
-    print(courses.json())
-    return templates.TemplateResponse("index.html", {"request": request})
+
+    try:
+        cookie_header = request.headers.get("cookie")[6:]
+        user = get_current_user(cookie_header)
+        courses = requests.get("http://localhost:8002/api/courses", headers={"AuThoRizaTion": f"Bearer {cookie_header}"})
+        courses = courses.json()
+
+
+        return templates.TemplateResponse("index.html", {"request": request, "user":user, "courses":courses})
+    
+    except:
+
+        courses = requests.get("http://localhost:8002/api/courses")
+        return templates.TemplateResponse("index.html", {"request": request, "courses":courses.json()})
+
 
 @frontend_router.get("/login")
 def login(request:Request):
@@ -28,3 +40,33 @@ def login(request:Request):
 def register(request:Request):
     return templates.TemplateResponse("register.html", {"request": request})
 
+
+@frontend_router.post("/")
+def process_form_data(request:Request, username: str = Form(...), password: str = Form(...), ):
+
+    user = authenticate_user(username,password)
+
+    if user is not None:
+        token = create_access_token({"sub":username})
+
+    user = get_current_user(token)
+
+    if user is not None:
+
+        courses = requests.get("http://localhost:8002/api/courses", headers={"AuThoRizaTion": f"Bearer {token}"})
+        response = templates.TemplateResponse("index.html", {"request": request, "user": user, "courses":courses.json()})
+        response.set_cookie(key="token", value=token)
+
+
+        return response
+    
+    else:
+        return templates.TemplateResponse("message.html", {"request": request, "message": "Login Invalid"})
+    
+
+@frontend_router.get("/logout", tags=["Frontend"])
+def logout(request: Request):
+
+    response = templates.TemplateResponse("message.html", {"request": request, "message":"Successfully Logged Out!"})
+    response.delete_cookie(key="token")
+    return response
--- app/main.py
+++ app/main.py
@@ -14,27 +14,8 @@ settings = get_settings()
 
 app = FastAPI()
 
-
 app.include_router(api_router)
 app.include_router(frontend_router)
 app.mount("/static", StaticFiles(directory="frontend/style"), name="static")
 app.mount("/assets", StaticFiles(directory="assets/course_thumbnails"), name="assets")
 
-
-@app.middleware("http")
-async def create_auth_header(request: Request, call_next,):
-    '''
-    Check if there are cookies set for authorization. If so, construct the
-    Authorization header and modify the request (unless the header already
-    exists!)
-    '''
-
-    if ("Authorization" not in request.headers and "Authorization" in request.cookies):
-
-        access_token = request.cookies["Authorization"]
-
-        request.headers.__dict__["_list"].append(
-            ( "authorization".encode(), f"Bearer {access_token}".encode(),))
-    
-    response = await call_next(request)
-    return response    
