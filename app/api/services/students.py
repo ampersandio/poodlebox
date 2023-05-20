260ce16dea7d02693869900f62624ce3edbae425
“newcopernicus” <alexandrova.blagovesta@gmail.com>
2023-05-20 19:41:34 +0300

    Change to functions

--- app/api/services/students.py
+++ app/api/services/students.py
@@ -8,6 +8,8 @@ def get_students_courses_id(student_id):
         "select group_concat(distinct courses_id) from users_has_courses where users_id=? group by users_id",
         (student_id,),
     )
+    if data==[]:
+        return []
     courses_ids = [x for x in data[0].split(",")]
     return courses_ids
 
