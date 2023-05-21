32e5b4052bf47d363bdd93aa822c4733f334b692
Nikolai Dimitrov <nikolaisyl@gmail.com>
2023-05-21 08:39:28 +0300

    refactor populate_db script

--- populate_db.sql
+++ populate_db.sql
@@ -1,89 +1,62 @@
 INSERT INTO users(email, first_name, last_name, password, phone_number, date_of_birth, verified_email, approved, role, linked_in_profile, disabled)
 VALUES("oldpepa@.", "pepa", "old", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "123-123-1234", "1889-06-07",1 , 1, 2, "linked.in/pepa_old", 0);
-
 INSERT INTO users(email, first_name, last_name, password, phone_number, date_of_birth, verified_email, approved, role, linked_in_profile, disabled)
 VALUES("olderpepa@.", "pepa", "older", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "987-987-9876", "1765-12-29",1 , 1, 2, "linked.in/pepa_older", 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled) 
 VALUES("gigaman@shiet.com", "gigaman", "returns", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "2000-01-29", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled) 
 VALUES("jack_rider@gmail.com", "jack", "rider", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1978-01-29", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled) 
 VALUES("steven_smith@gmail.com", "steven", "smith", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1998-08-21", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("jennifer_walker@gmail.com", "jennifer", "walker", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1985-03-12", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("david_anderson@gmail.com", "david", "anderson", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1973-11-05", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("emily_jones@gmail.com", "emily", "jones", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1992-06-15", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("alexander_brown@gmail.com", "alexander", "brown", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1987-09-03", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("sarah_clark@gmail.com", "sarah", "clark", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1995-02-18", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("matthew_davis@gmail.com", "matthew", "davis", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1982-12-09", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("olivia_harris@gmail.com", "olivia", "harris", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1990-07-24", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("jacob_thomas@gmail.com", "jacob", "thomas", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1999-04-01", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("mia_rodriguez@gmail.com", "mia", "rodriguez", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1984-10-14", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("natalie_wilson@gmail.com", "natalie", "wilson", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1991-11-27", 0, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("liam_andrews@gmail.com", "liam", "andrews", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1996-09-08", 0, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("samantha_miller@gmail.com", "samantha", "miller", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1993-04-02", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("benjamin_carter@gmail.com", "benjamin", "carter", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1989-12-17", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("ava_turner@gmail.com", "ava", "turner", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1997-07-09", 1, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("daniel_roberts@gmail.com", "daniel", "roberts", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1986-08-12", 0, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("emma_wright@gmail.com", "emma", "wright", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1994-03-25", 0, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("noah_martin@gmail.com", "noah", "martin", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1990-10-08", 0, 1, 0);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("william_harris@gmail.com", "william", "harris", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1988-07-07", 1, 1, 1);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("abigail_nelson@gmail.com", "abigail", "nelson", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1993-11-11", 1, 1, 1);
-
 INSERT INTO users(email, first_name, last_name, password, date_of_birth, verified_email, role, disabled)
 VALUES("james_wright@gmail.com", "james", "wright", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "1991-04-30", 1, 1, 1);
-
 INSERT INTO users(email, first_name, last_name, password, phone_number, date_of_birth, verified_email, approved, role, linked_in_profile, disabled)
 VALUES("tamara_smith34@gmail.com", "tamara", "smith", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "176-927-8894", "1989-06-07",1 , 1, 2, "linked.in/tamara_smith", 0);
-
 INSERT INTO users(email, first_name, last_name, password, phone_number, date_of_birth, verified_email, approved, role, linked_in_profile, disabled)
 VALUES("usein_petrov58@gmail.com", "usein", "petrov", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "703-618-3752", "1965-12-29",1 , 1, 2, "linked.in/husein_petrov", 0);
-
 INSERT INTO users(email, first_name, last_name, password, phone_number, date_of_birth, verified_email, approved, role, linked_in_profile, disabled)
 VALUES("jennifer_white83@gmail.com", "jennifer", "white", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "512-789-4531", "1992-07-15", 1, 1, 2, "linked.in/jennifer_white", 0);
-
 INSERT INTO users(email, first_name, last_name, password, phone_number, date_of_birth, verified_email, approved, role, linked_in_profile, disabled)
 VALUES("robert_johnson21@gmail.com", "robert", "johnson", "$2b$12$9y2kGbqPPdbjJldzEid3feSVN04csLKcAOV9BSQdSJ7CFFmcddQ96", "987-654-3210", "1987-03-18", 1, 1, 2, "linked.in/robert_johnson", 0);
+
 
 INSERT INTO sections(title, courses_id) VALUES("OOP", 6);
 INSERT INTO sections(title, courses_id) VALUES("DSA", 6);
@@ -94,6 +67,7 @@ INSERT INTO sections(title, courses_id) VALUES("DSA", 9);
 INSERT INTO sections(title, courses_id) VALUES("Potting", 8);
 INSERT INTO sections(title, courses_id) VALUES("Watering", 8);
 INSERT INTO sections(title, courses_id) VALUES("Flowering", 8);
+
 
 INSERT INTO users_has_sections(users_id, sections_id) VALUES(2, 1);
 INSERT INTO users_has_sections(users_id, sections_id) VALUES(7, 4);
@@ -120,7 +94,10 @@ INSERT INTO users_has_sections(users_id, sections_id) VALUES(27, 8);
 INSERT INTO users_has_sections(users_id, sections_id) VALUES(27, 9);
 INSERT INTO users_has_sections(users_id, sections_id) VALUES(27, 10);
 
-INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(2, 6, 1);
+
+INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(2, 6, 3);
+INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(2, 8, 1);
+INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(2, 9, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(3, 6, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(5, 7, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(6, 6, 2);
@@ -129,9 +106,11 @@ INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(9, 
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(10, 7, 2);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(11, 8, 3);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(12, 9, 1);
+INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(12, 8, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(13, 6, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(14, 7, 3);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(15, 8, 1);
+INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(15, 9, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(16, 9, 2);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(17, 6, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(18, 7, 2);
@@ -141,6 +120,7 @@ INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(21,
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(22, 7, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(26, 8, 2);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(27, 9, 1);
+INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(27, 8, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(29, 6, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(5, 8, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(6, 7, 2);
@@ -160,8 +140,7 @@ INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(21,
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(22, 6, 1);
 INSERT INTO users_has_courses(users_id, courses_id, subscriptions_id) VALUES(26, 7, 2);
 
-INSERT INTO reviews(users_id, courses_id, rating, description) VALUES(9, 6, 6.2, "it was alright");
-INSERT INTO reviews(users_id, courses_id, rating, description) VALUES(5, 7, 3.5, "certainly not my favorite course ever");
+
 INSERT INTO reviews(users_id, courses_id, rating, description) VALUES(2, 6, 7.6, "Very informative course.");
 INSERT INTO reviews(users_id, courses_id, rating, description) VALUES(3, 6, 5, "Not what I expected.");
 INSERT INTO reviews(users_id, courses_id, rating, description) VALUES(9, 6, 6, "Interesting content.");
