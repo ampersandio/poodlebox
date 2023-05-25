7a3d6baf39cb5db3b3e96262644715059d0b47bb
“newcopernicus” <alexandrova.blagovesta@gmail.com>
2023-05-25 16:00:12 +0300

    Bug fix

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 from api.routers.authorization import authorization_router
 from api.routers.courses_router import courses_router
 from api.routers.students_router import students_router
-from api.routers.report import report_router
 from api.routers.admins_router import admins_router
 from api.routers.teachers_router import teachers_router
 from api.routers.ads_router import ads_router
@@ -12,7 +11,6 @@ api_router = APIRouter(prefix="/api")
 api_router.include_router(authorization_router)
 api_router.include_router(courses_router)
 api_router.include_router(students_router)
-api_router.include_router(report_router)
 api_router.include_router(admins_router)
 api_router.include_router(ads_router)
 api_router.include_router(teachers_router)
