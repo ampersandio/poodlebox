952f680f596d23ef7eb76cdffca463934419ea06
“newcopernicus” <alexandrova.blagovesta@gmail.com>
2023-05-29 23:39:32 +0300

    Ads router fixes

--- app/api/routers/ads_router.py
+++ app/api/routers/ads_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from api.data.models import User
 from api.services.authorization import get_current_user
-from api.services.users import view_add
+from api.services.users import view_ad
 
 ads_router = APIRouter(prefix="/ads", tags=["Ads"])
 
 @ads_router.get("/")
 def get_users_ad(current_user: User = Depends(get_current_user)):
-    return view_add(current_user.id)
+    return view_ad(current_user.id)
