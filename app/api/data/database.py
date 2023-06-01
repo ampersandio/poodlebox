ceba9c2e30ba3e1297304f540bdf9853474b71a9
Nikolai Dimitrov <nikolaisyl@gmail.com>
2023-06-01 14:30:21 +0300

    refactor: update_query returns rowscount

--- app/api/data/database.py
+++ app/api/data/database.py
@@ -42,6 +42,8 @@ def update_query(sql: str, sql_params: tuple = ()) -> list:
         cursor.execute(sql, sql_params)
         connection.commit()
 
+        return cursor.rowcount
+
     
 def delete_query(sql: str, sql_params: tuple = ()) -> list:
     with _get_connection() as connection:
