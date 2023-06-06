471d90206ef1576918c4894188403d83a3f80f43
Blagovesta Aleksandrova <alexandrova.blagovesta@gmail.com>
2023-06-06 09:57:02 +0000

    Revert "Merge branch 'blago' into 'main'"
    This reverts merge request !66

--- app/tests/calendars_services_test.py
+++ app/tests/calendars_services_test.py
@@ -2,7 +2,6 @@ import unittest
 from api.services import calendars
 from api.data.models import Calendar, Event, CalendarChange, CalendarCreate, EventChange, EventCreate
 
-#HHHAHAHAHAA
 
 class CalendarServiceShould(unittest.TestCase):
     def test_get_all_calendars_studentsWithData(self):
